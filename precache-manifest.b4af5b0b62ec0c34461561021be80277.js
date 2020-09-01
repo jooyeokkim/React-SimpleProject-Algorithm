92b22a1134ba29289517b5063fb195f5ef874cd5
jooyeokkim <67864645+jooyeokkim@users.noreply.github.com>
2020-09-01 09:41:55 +0900

    Updates

---
+++ precache-manifest.b4af5b0b62ec0c34461561021be80277.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f761d31563a657ec31f329586399a6b0",
+    "url": "/react-test2/index.html"
+  },
+  {
+    "revision": "6490a486a2346a763e52",
+    "url": "/react-test2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0babbde14e333bc689a2",
+    "url": "/react-test2/static/js/2.8aaac6e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-test2/static/js/2.8aaac6e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6490a486a2346a763e52",
+    "url": "/react-test2/static/js/main.0b13ff1c.chunk.js"
+  },
+  {
+    "revision": "4bb2966fb8efcfe97e61",
+    "url": "/react-test2/static/js/runtime-main.79e79a41.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test2/static/media/logo.5d5d9eef.svg"
+  }
+]);
