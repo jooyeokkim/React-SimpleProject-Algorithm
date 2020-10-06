eb924609eed85c010de5d9e3fe93769a52e1dc25
jooyeokkim <67864645+jooyeokkim@users.noreply.github.com>
2020-10-06 13:52:57 +0900

    20201006

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Dynamic from './algorithms/dynamic';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!!!!!
-        </a>
-      </header>
+      <html lang="en">
+        <head>
+          <meta charset="utf-8" />
+          <title>React App</title>
+        </head>
+        <body>
+          <Dynamic></Dynamic>
+        </body>
+      </html>
     </div>
   );
 }
