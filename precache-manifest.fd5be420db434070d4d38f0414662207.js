2695af43d8ede4560056b2333ef2ef3db05a7924
jooyeokkim <67864645+jooyeokkim@users.noreply.github.com>
2021-03-10 14:06:15 +0900

    Updates

---
+++ precache-manifest.fd5be420db434070d4d38f0414662207.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "141b35318e36af31475c39e0150f5545",
+    "url": "/React-SimpleProject-Algorithm/index.html"
+  },
+  {
+    "revision": "0219241d6485a1c3301c",
+    "url": "/React-SimpleProject-Algorithm/static/css/main.3cba6558.chunk.css"
+  },
+  {
+    "revision": "8a14d3fcba8c6c246512",
+    "url": "/React-SimpleProject-Algorithm/static/js/2.56bd2944.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-SimpleProject-Algorithm/static/js/2.56bd2944.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0219241d6485a1c3301c",
+    "url": "/React-SimpleProject-Algorithm/static/js/main.de29c21d.chunk.js"
+  },
+  {
+    "revision": "0d7a7d173f5cfae6a1f7",
+    "url": "/React-SimpleProject-Algorithm/static/js/runtime-main.21328662.js"
+  },
+  {
+    "revision": "fec89ee875f57620f9cb246298391291",
+    "url": "/React-SimpleProject-Algorithm/static/media/algorithm.fec89ee8.jpg"
+  },
+  {
+    "revision": "a96c04573de4eeef61efd6c4258656cf",
+    "url": "/React-SimpleProject-Algorithm/static/media/bfs.a96c0457.jpg"
+  },
+  {
+    "revision": "3fea7b9e3edd6ee3c995de9e06577d0c",
+    "url": "/React-SimpleProject-Algorithm/static/media/bruteforce.3fea7b9e.jpg"
+  },
+  {
+    "revision": "0a9eba7703515c9fde7d10243bc41969",
+    "url": "/React-SimpleProject-Algorithm/static/media/dfs.0a9eba77.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-test2/precache-manifest.b6e447ae5f6a2e3a97b74f15ea1d20f0.js"
+  "/React-SimpleProject-Algorithm/precache-manifest.fd5be420db434070d4d38f0414662207.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-test2/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-SimpleProject-Algorithm/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
